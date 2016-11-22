4e69f2551f4c01384e92008ec63847d40759d687
Tim Vermeulen <tim@Tims-MacBook-Pro.local>
2016-11-22 05:39:56 +0100

    Improved testing

--- Tests/DraughtsTests/DraughtsTests.swift
+++ Tests/DraughtsTests/DraughtsTests.swift
@@ -1,7 +1,28 @@
 import XCTest
 @testable import Draughts
 
+let errorMessage = "`continueAfterFailure` should be set to `false` inside `setUp()`, and set to `true` inside `tearDown()`"
+
+func XCTFatal(_ message: String = "", file: StaticString = #file, line: UInt = #line) -> Never {
+    XCTFail(message, file: file, line: line)
+    fatalError(errorMessage)
+}
+
+func XCTUnwrap<T>(_ expression: @autoclosure () throws -> T?, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> T {
+    XCTAssertNotNil(try expression(), message())
+    guard let result = (try? expression()) ?? nil else { fatalError(errorMessage) }
+    return result
+}
+
 class DraughtsTests: XCTestCase {
+    override func setUp() {
+        self.continueAfterFailure = false
+    }
+    
+    override func tearDown() {
+        self.continueAfterFailure = true
+    }
+    
     func testOpeningMoves() {
         let position = Position.beginPosition
         
@@ -15,16 +36,16 @@ class DraughtsTests: XCTestCase {
         var position = Position.beginPosition
         
         let openingMoves = position.generateMoves()
-        guard let firstMove = openingMoves.first(where: { $0.notation == "32-28" }) else { XCTFail(); return }
+        let firstMove = XCTUnwrap(openingMoves.first(where: { $0.notation == "32-28" }))
         position.play(firstMove)
         
         let returnMoves = position.generateMoves()
-        guard let secondMove = returnMoves.first(where: { $0.notation == "19-23" }) else { XCTFail(); return }
+        let secondMove = XCTUnwrap(returnMoves.first(where: { $0.notation == "19-23" }))
         position.play(secondMove)
         
         let returnReturnMoves = position.generateMoves()
+        let capture = XCTUnwrap(returnReturnMoves.first)
         XCTAssertEqual(returnReturnMoves.count, 1)
-        let capture = returnReturnMoves.first!
         XCTAssertTrue(capture.isCapture)
         
         position.play(capture)
@@ -35,19 +56,32 @@ class DraughtsTests: XCTestCase {
     
     func testFEN() {
         let fen = "W:W33:BK28"
-        guard let position = Position(fen: fen) else { XCTFail(); return }
-        XCTAssertEqual(position, Position(pieces: [Piece(player: .white, kind: .man, square: 33), Piece(player: .black, kind: .king, square: 28)]))
+        
+        let position = XCTUnwrap(Position(fen: fen))
+        let expected = Position(
+            pieces: [
+                Piece(player: .white, kind: .man, square: 33),
+                Piece(player: .black, kind: .king, square: 28)
+            ]
+        )
+        
+        XCTAssertEqual(position, expected)
     }
     
     func testCoupTurc() {
         let fen = "W:WK26:B9,12,13,23,24"
-        guard let position = Position(fen: fen) else { XCTFail(); return }
+        let position = XCTUnwrap(Position(fen: fen))
         let moves = position.generateMoves()
-        guard let move = moves.first, moves.count == 1 else { XCTFail(); return }
+        
+        let move = XCTUnwrap(moves.first)
+        XCTAssertEqual(moves.count, 1)
         
         let next = position.playing(move: move)
         let expected = Position(
-            pieces: [Piece(player: .white, kind: .king, square: 18), Piece(player: .black, kind: .man, square: 13)],
+            pieces: [
+                Piece(player: .white, kind: .king, square: 18),
+                Piece(player: .black, kind: .man, square: 13)
+            ],
             ply: Ply(player: .black)
         )
         
