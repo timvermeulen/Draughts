2fed9496e8147da3b242eafc6cbf76ec25ec4e66
Tim Vermeulen <tim@ip-145-116-155-193.wlan-int.ru.nl>
2016-11-29 19:04:41 +0100

    Improve move validation check

--- Sources/Position.swift
+++ Sources/Position.swift
@@ -73,7 +73,7 @@ public final class Position {
     // MARK: Moves
     
     public func moveIsValid(_ move: Move) -> Bool {
-        return move.startPosition === self
+        return move.startPosition == self
     }
     
     internal func moves(of player: Player) -> [Move] {
