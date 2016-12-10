05782d45be5cf74ffbf0e3f2fdf010ad2461d573
Tim Vermeulen <tim@Tims-MBP.fritz.box>
2016-12-10 21:33:32 +0100

    Refactored Ply compatibility check

--- Sources/Ply.swift
+++ Sources/Ply.swift
@@ -17,18 +17,36 @@ extension Ply {
     }
 }
 
+extension Ply {
+    fileprivate func isCompatible(with other: Ply) -> Bool {
+        let offset = other.number - self.number
+        
+        let offsetIsEven = offset % 2 == 0
+        let playersEqual = self.player == other.player
+        
+        return offsetIsEven == playersEqual
+    }
+    
+    fileprivate func compatibilityCheck(with other: Ply) {
+        assert(self.isCompatible(with: other), "\(self) is incompatible with \(other)")
+    }
+}
+
 extension Ply: Comparable {
     public static func < (left: Ply, right: Ply) -> Bool {
+        left.compatibilityCheck(with: right)
         return left.number < right.number
     }
     
     public static func == (left: Ply, right: Ply) -> Bool {
+        left.compatibilityCheck(with: right)
         return left.number == right.number
     }
 }
 
 extension Ply: Strideable {
     public func advanced(by offset: Int) -> Ply {
+        assert(self.number + offset >= 0)
         return Ply(
             player: offset % 2 == 0 ? self.player : self.player.opponent,
             number: self.number + offset
@@ -36,11 +54,12 @@ extension Ply: Strideable {
     }
     
     public func distance(to other: Ply) -> Int {
-        assert((self.number - other.number % 2 == 0) == (self.player == other.player), "`self` is incompatible with `other`")
+        self.compatibilityCheck(with: other)
         return other.number - self.number
     }
     
     public var predecessor: Ply {
+        assert(self.number > 0)
         return Ply(player: self.player.opponent, number: self.number - 1)
     }
     
