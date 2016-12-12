455ed2d2be7bf0cedc82cb324489093ecd23be21
Tim Vermeulen <tim@Tims-MBP.fritz.box>
2016-12-12 13:29:44 +0100

    Fixed sequence init of Dictionary

--- Sources/Dictionary+initWithSequence.swift
+++ Sources/Dictionary+initWithSequence.swift
@@ -1,5 +1,5 @@
 extension Dictionary {
-    internal init<S: Sequence>(_ elements: S) where S.Iterator.Element == (Key, Value?) {
+    internal init<S: Sequence>(_ elements: S) where S.Iterator.Element == (Key, Value) {
         self.init()
         
         for (key, value) in elements {
