782798db17c96382bb8f9b3835270fb41e7dd25b
Tim Vermeulen <tim@Tims-MBP.fritz.box>
2017-01-10 13:40:30 +0100

    Improved test functions

--- Tests/DraughtsTests/SafeXCTest.swift
+++ Tests/DraughtsTests/SafeXCTest.swift
@@ -1,27 +1,5 @@
 import XCTest
 
-public func fail(_ message: String = "", file: StaticString = #file, line: UInt = #line) -> Never {
-    XCTFail(message, file: file, line: line)
-    fatalError("Make sure to subclass `TestCase` instead of `XCTestCase`")
-}
-
-public func unwrapOrFail<T>(_ expression: @autoclosure () -> T?, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> T {
-    let result = expression()
-    XCTAssertNotNil(result, message(), file: file, line: line)
-    
-    guard let unwrapped = result else { fail(message(), file: file, line: line) }
-    return unwrapped
-}
-
-public func tryOrFail<T>(_ expression: @autoclosure () throws -> T, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> T {
-    let result: T
-    
-    do { result = try expression() }
-    catch { fail(message(), file: file, line: line) }
-    
-    return result
-}
-
 open class TestCase: XCTestCase {
     override open func setUp() {
         super.setUp()
@@ -31,5 +9,28 @@ open class TestCase: XCTestCase {
     override open func tearDown() {
         self.continueAfterFailure = true
         super.tearDown()
+    }
+}
+
+extension TestCase {
+    private func shouldNotHappen() -> Never {
+        fatalError("`continueAfterFailure` should not be set to `true`")
+    }
+    
+    public func fail(_ message: String = "", file: StaticString = #file, line: UInt = #line) -> Never {
+        XCTFail(message, file: file, line: line)
+        shouldNotHappen()
+    }
+    
+    public func unwrapOrFail<T>(_ expression: @autoclosure () -> T?, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line) -> T {
+        let optional = expression()
+        XCTAssertNotNil(optional, message(), file: file, line: line)
+        guard let result = optional else { shouldNotHappen() }
+        return result
+    }
+    
+    public func tryOrFail<T>(_ expression: @autoclosure () throws -> T, file: StaticString = #file, line: UInt = #line, _ message: (Error) -> String = String.init(describing:)) -> T {
+        do { return try expression() }
+        catch { fail(message(error), file: file, line: line) }
     }
 }
